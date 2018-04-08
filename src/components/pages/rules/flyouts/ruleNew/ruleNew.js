e844af84866bd60d099d01394df6f0edc5d76424
Isaac <jojosep@microsoft.com>
2018-04-08 16:29:56 -0700

    rule UI changes

--- src/components/pages/rules/flyouts/ruleNew/ruleNew.js
+++ src/components/pages/rules/flyouts/ruleNew/ruleNew.js
@@ -6,8 +6,12 @@ import {
   BtnToolbar,
   FormControl,
   FormGroup,
-  FormLabel
+  FormLabel,
+  Radio,
+  Svg,
+
 } from 'components/shared';
+import { SeverityRenderer } from 'components/shared/cellRenderers';
 import { svgs, LinkedComponent } from 'utilities';
 import Flyout from 'components/shared/flyout';
 
@@ -18,11 +22,29 @@ const Section = Flyout.Section;
 // A counter for creating unique keys per new condition
 let conditionKey = 0;
 
+//Creates a state object for time period
+const newTimePeriod = () => ({
+  hours: '',
+  minutes: '',
+  seconds: ''
+})
+
 // Creates a state object for a condition
 const newCondition = () => ({
+  field: '',
+  calculation: '',
+  timePeriod: [newTimePeriod()],
+  operator: '',
   value: '',
   key: conditionKey++ // Used by react to track the rendered elements
 });
+
+//Creates a state object for severity level
+const newSeverityLevel = () => ({
+  critical: true,
+  warning: false,
+  info: false
+})
 
 // TODO: Translate all the hard coded strings
 export class RuleNew extends LinkedComponent {
@@ -33,16 +55,28 @@ export class RuleNew extends LinkedComponent {
     this.state = {
       name: '',
       description: '',
-      conditions: [ newCondition() ] // Start with one condition
+      deviceGroup: '',
+      conditions: [newCondition()], // Start with one condition
+      severityLevel: {
+        critical: true,
+        warning: false,
+        info: false
+      },
+      ruleStatus: false,
+      devicesAffected: 0
     };
 
     // State links
     this.ruleName = this.linkTo('name');
     this.description = this.linkTo('description');
+    this.deviceGroup = this.linkTo('deviceGroup');
     this.conditions = this.linkTo('conditions');
+    this.severityLevel = this.linkTo('severityLevel');
+    // this.ruleStatus = this.linkTo('ruleStatus');
+    // this.devicesAffected = this.linkTo
   }
 
-  addCondition = () => this.conditions.set([ ...this.conditions.value, newCondition() ]);
+  addCondition = () => this.conditions.set([...this.conditions.value, newCondition()]);
 
   deleteCondition = (index) =>
     () => this.conditions.set(this.conditions.value.filter((_, idx) => index !== idx));
@@ -53,7 +87,7 @@ export class RuleNew extends LinkedComponent {
   }
 
   render() {
-    const { onClose } = this.props;
+    const { onClose, t } = this.props;
 
     // Create the state link for the dynamic form elements
     const conditionLinks = this.conditions.getLinkedChildren(conditionLink => {
@@ -71,20 +105,28 @@ export class RuleNew extends LinkedComponent {
           <form onSubmit={this.createRule}>
             <Section.Container className="rule-property-container">
               <Section.Content>
-              <FormGroup>
-                <FormLabel>Rule name</FormLabel>
-                <FormControl
-                  type="text"
-                  placeholder="Rule name"
-                  link={this.ruleName} />
-              </FormGroup>
-              <FormGroup>
-                <FormLabel>Description</FormLabel>
-                <FormControl
-                  type="textarea"
-                  placeholder="Description"
-                  link={this.description} />
-              </FormGroup>
+                <FormGroup>
+                  <FormLabel>Rule name</FormLabel>
+                  <FormControl
+                    type="text"
+                    placeholder="Rule name"
+                    link={this.ruleName} />
+                </FormGroup>
+                <FormGroup>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl
+                    type="textarea"
+                    placeholder="Description"
+                    link={this.description} />
+                </FormGroup>
+                <FormGroup>
+                  <FormLabel>Device Group</FormLabel>
+                  <FormControl
+                    type="select"
+                    className="long"
+                    placeholder="DeviceGroup"
+                    link={this.deviceGroup} />
+                </FormGroup>
               </Section.Content>
             </Section.Container>
 
@@ -99,23 +141,87 @@ export class RuleNew extends LinkedComponent {
                 <Section.Container key={this.state.conditions[idx].key}>
                   <Section.Header>Condition {idx + 1}</Section.Header>
                   <Section.Content>
-                  {
-                    conditionLinks.length > 1 &&
-                    <Btn svg={svgs.trash} onClick={this.deleteCondition(idx)}>Delete</Btn>
-                  }
-                  <FormGroup>
-                    <FormLabel>Value</FormLabel>
-                    <FormControl
-                      type="text"
-                      placeholder="Value"
-                      link={condition.value} />
-                  </FormGroup>
+                    {
+                      conditionLinks.length > 1 &&
+                      <Btn svg={svgs.trash} onClick={this.deleteCondition(idx)}>Delete</Btn>
+                    }
+                    <FormGroup>
+                      <FormLabel>Field</FormLabel>
+                      <FormControl
+                        type="select"
+                        className="long"
+                        placeholder="Select field"
+                        link={condition.field} />
+                    </FormGroup>
+                    <FormGroup>
+                      <FormLabel>Calculation</FormLabel>
+                      <FormControl
+                        type="select"
+                        className="long"
+                        placeholder="Select"
+                        link={condition.calculation} />
+                    </FormGroup>
+                    <FormGroup>
+                      <FormLabel>Time Period</FormLabel>
+                      <FormControl
+                        type="duration"
+                        className="long"
+                        placeholder="Select"
+                        link={condition.timePeriod} />
+                    </FormGroup>
+                    <FormGroup>
+                      <FormLabel>Operator</FormLabel>
+                      <FormControl
+                        type="select"
+                        className="short"
+                        placeholder="Select operator"
+                        link={condition.operator} />
+                    </FormGroup>
+                    <FormGroup>
+                      <FormLabel>Value</FormLabel>
+                      <FormControl
+                        type="text"
+                        placeholder="Enter value"
+                        link={condition.value} />
+                    </FormGroup>
                   </Section.Content>
                 </Section.Container>
               ))
             }
+            <Section.Container collapsable={false}>
+              <Section.Header>Severity level</Section.Header>
+              <Section.Content>
+                <FormGroup>
+                  <Radio
+                    placeholder="critical"
+                    checked={this.state.severityLevel.critical}>
+                    <SeverityRenderer value="Critical" context={{ t }} iconOnly={false} />
+                  </Radio>
+                  <Radio
+                    placeholder="warning"
+                    checked={this.state.severityLevel.warning} >
+                    <SeverityRenderer value="Warning" context={{ t }} iconOnly={false} />
+                  </Radio>
+                  <Radio
+                    placeholder="info"
+                    checked={this.state.severityLevel.info} >
+                    <SeverityRenderer value="Info" context={{ t }} iconOnly={false} />
+                  </Radio>
+                </FormGroup>
+              </Section.Content>
+              <Section.Header>Rule status</Section.Header>
+              <Section.Content>
+                <Btn svg={svgs.disabled} onClick={this.addCondition}>Enabled</Btn>
+              </Section.Content>
+            </Section.Container>
+            <Section.Container collapsable={false}>
+              <Section.Header>Severity level</Section.Header>
+              <Section.Content>
+              </Section.Content>
+            </Section.Container>
             <BtnToolbar>
-              <Btn type="submit">Submit</Btn>
+              <Btn type="submit">Apply</Btn>
+              <Btn type="submit">Cancel</Btn>
             </BtnToolbar>
           </form>
         </Flyout.Content>
